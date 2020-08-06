63d8b5a4257c9d6a9728cb64b8d50cf37c0ff070
Amen <2805478472@qq.com>
2020-08-06 16:22:30 +0800

    修改model的save属性

--- blog/models.py
+++ blog/models.py
@@ -31,9 +31,9 @@ class Tag(models.Model):
 class Post(models.Model):
     title = models.CharField('标题', max_length=70)
     body = models.TextField()
-    created_time = models.DateTimeField()
+    created_time = models.DateTimeField('创建时间', default=timezone.now)
     modified_time = models.DateTimeField('修改时间')
-    excerpt = models.CharField( max_length=200, blank=True)
+    excerpt = models.CharField('摘要', max_length=200, blank=True)
     category = models.ForeignKey(Category, verbose_name='分类', on_delete=models.CASCADE)
     tags = models.ManyToManyField(Tag, verbose_name='标签', blank=True)
     author = models.ForeignKey(User, verbose_name='作者', on_delete=models.CASCADE)
@@ -58,7 +58,7 @@ class Post(models.Model):
     class Meta:
         verbose_name = '文章'
         verbose_name_plural = verbose_name
-        ordering = ['-created_time']
+
 
     def __str__(self):
         return self.title
