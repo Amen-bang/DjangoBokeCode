3d8e5b46e7a43dc21f3ac423dcdfaa2b246679b5
Amen <2805478472@qq.com>
2020-08-06 17:03:48 +0800

    修改了阅读量属性

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render,get_object_or_404
 from .models import Post,Category,Tag
+from django.shortcuts import render,get_object_or_404
 import markdown
+from markdown.extensions.toc import TocExtension, slugify
+import re
+
 
 def index(request):
     post_list = Post.objects.all().order_by('-created_time')
@@ -9,18 +12,26 @@ def index(request):
     }
     return render(request, 'blog/index.html',context=context)
 
+
 def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    context={
-    	'post': post
-    }
-    post.body = markdown.markdown(post.body,
-                                  extensions=[
-                                      'markdown.extensions.extra',
-                                      'markdown.extensions.codehilite',
-                                      'markdown.extensions.toc',
-                                  ])
-    return render(request, 'blog/detail.html', context=context)
+
+    # 阅读量 +1
+    post.increase_views()
+
+    md = markdown.Markdown(extensions=[
+        'markdown.extensions.extra',
+        'markdown.extensions.codehilite',
+        # 记得在顶部引入 TocExtension 和 slugify
+        TocExtension(slugify=slugify),
+    ])
+    post.body = md.convert(post.body)
+
+    m = re.search(r'<div class="toc">\s*<ul>(.*)</ul>\s*</div>', md.toc, re.S)
+    post.toc = m.group(1) if m is not None else ''
+
+    return render(request, 'blog/detail.html', context={'post': post})
+
 
 def archive(request, year, month):
     post_list = Post.objects.filter(created_time__year=year,
@@ -31,11 +42,13 @@ def archive(request, year, month):
     }
     return render(request, 'blog/index.html', context=context)
 
+
 def category(request, pk):
     # 记得在开始部分导入 Category 类
     cate = get_object_or_404(Category, pk=pk)
     post_list = Post.objects.filter(category=cate).order_by('-created_time')
     return render(request, 'blog/index.html', context={'post_list': post_list})
+
 
 def tag(request, pk):
     # 记得在开始部分导入 Tag 类
