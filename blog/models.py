ee12c1f87ff167a30ddbf16063bcb3e1238b4766
Amen <2805478472@qq.com>
2020-08-07 10:26:11 +0800

    开启 Django 博客的 RSS 功能

--- blog/models.py
+++ blog/models.py
@@ -8,6 +8,20 @@ from django.utils.html import strip_tags
 from markdown.extensions.toc import TocExtension, slugify
 from django.utils.functional import cached_property
 
+def generate_rich_content(value):
+    md = markdown.Markdown(
+        extensions=[
+            "markdown.extensions.extra",
+            "markdown.extensions.codehilite",
+            # 记得在顶部引入 TocExtension 和 slugify
+            TocExtension(slugify=slugify),
+        ]
+    )
+    content = md.convert(value)
+    m = re.search(r'<div class="toc">\s*<ul>(.*)</ul>\s*</div>', md.toc, re.S)
+    toc = m.group(1) if m is not None else ""
+    return {"content": content, "toc": toc}
+
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
@@ -32,25 +46,39 @@ class Tag(models.Model):
 
 
 class Post(models.Model):
-    title = models.CharField('标题', max_length=70)
-    body = models.TextField()
-    created_time = models.DateTimeField('创建时间', default=timezone.now)
-    modified_time = models.DateTimeField('修改时间')
-    excerpt = models.CharField('摘要', max_length=200, blank=True)
-    category = models.ForeignKey(Category, verbose_name='分类', on_delete=models.CASCADE)
-    tags = models.ManyToManyField(Tag, verbose_name='标签', blank=True)
-    author = models.ForeignKey(User, verbose_name='作者', on_delete=models.CASCADE)
+    """
+    文章的数据库表稍微复杂一点，主要是涉及的字段更多。
+    """
+
+    # 文章标题
+    title = models.CharField("标题", max_length=70)
+    body = models.TextField("正文")
+    created_time = models.DateTimeField("创建时间", default=timezone.now)
+    modified_time = models.DateTimeField("修改时间")
+    excerpt = models.CharField("摘要", max_length=200, blank=True)
+    category = models.ForeignKey(Category, verbose_name="分类", on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag, verbose_name="标签", blank=True)
+    author = models.ForeignKey(User, verbose_name="作者", on_delete=models.CASCADE)
+
+    # 新增 views 字段记录阅读量
     views = models.PositiveIntegerField(default=0, editable=False)
+
+    class Meta:
+        verbose_name = "文章"
+        verbose_name_plural = verbose_name
+        ordering = ["-created_time"]
+
+    def __str__(self):
+        return self.title
 
     def save(self, *args, **kwargs):
         self.modified_time = timezone.now()
 
         # 首先实例化一个 Markdown 类，用于渲染 body 的文本。
         # 由于摘要并不需要生成文章目录，所以去掉了目录拓展。
-        md = markdown.Markdown(extensions=[
-            'markdown.extensions.extra',
-            'markdown.extensions.codehilite',
-        ])
+        md = markdown.Markdown(
+            extensions=["markdown.extensions.extra", "markdown.extensions.codehilite",]
+        )
 
         # 先将 Markdown 文本渲染成 HTML 文本
         # strip_tags 去掉 HTML 文本的全部 HTML 标签
@@ -59,22 +87,14 @@ class Post(models.Model):
 
         super().save(*args, **kwargs)
 
-    class Meta:
-        verbose_name = '文章'
-        verbose_name_plural = verbose_name
-
-
-    def __str__(self):
-        return self.title
-        # 自定义 get_absolute_url 方法
-        # 记得从 django.urls 中导入 reverse 函数
-
+    # 自定义 get_absolute_url 方法
+    # 记得从 django.urls 中导入 reverse 函数
     def get_absolute_url(self):
-        return reverse('blog:detail', kwargs={'pk': self.pk})
+        return reverse("blog:detail", kwargs={"pk": self.pk})
 
     def increase_views(self):
         self.views += 1
-        self.save(update_fields=['views'])
+        self.save(update_fields=["views"])
 
     @property
     def toc(self):
@@ -89,16 +109,4 @@ class Post(models.Model):
         return generate_rich_content(self.body)
 
 
-def generate_rich_content(value):
-    md = markdown.Markdown(
-        extensions=[
-            "markdown.extensions.extra",
-            "markdown.extensions.codehilite",
-            # 记得在顶部引入 TocExtension 和 slugify
-            TocExtension(slugify=slugify),
-        ]
-    )
-    content = md.convert(value)
-    m = re.search(r'<div class="toc">\s*<ul>(.*)</ul>\s*</div>', md.toc, re.S)
-    toc = m.group(1) if m is not None else ""
-    return {"content": content, "toc": toc}
+
