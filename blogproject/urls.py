31e6aa9f1d4d68eba2bec6452875f064a536b399
Amen <2805478472@qq.com>
2020-08-07 10:00:55 +0800

    开启 Django 博客的 RSS 功能

--- blogproject/urls.py
+++ blogproject/urls.py
@@ -15,10 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from blog.feeds import AllPostsRssFeed
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     path('', include('comments.urls')),
+
+    path('all/rss/', AllPostsRssFeed(), name='rss'),
 ]
 
